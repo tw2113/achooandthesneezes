1c62c8472b2113efc8684b2bcc7c11135656fcb1
Michael Beckwith <tw2113@gmail.com>
2019-03-09 19:19:44 -0600

    Output our document status

--- themes/octothorpe-child/single.php
+++ themes/octothorpe-child/single.php
@@ -17,14 +17,11 @@ while ( have_posts() ) : the_post(); ?>
 				<p><?php echo get_the_category_list( ', ' ); ?></p>
 			<?php } ?>
 			<?php the_title( '<header><h1>', '</h1></header>' ); ?>
-			<?php if ( has_post_thumbnail() ) { ?>
-				<figure>
-					<?php the_post_thumbnail();
-					$post_excerpt = get_post( get_post_thumbnail_id() )->post_excerpt;
-					if ( $post_excerpt ) { ?>
-						<figcaption><?php echo $post_excerpt; ?></figcaption>
-					<?php } ?>
-				</figure>
+			<?php
+			$status = get_post_meta( get_the_ID(), '_cwr_docstate', true );
+
+			if ( $status ) { ?>
+				<p>Document status: <strong><?php echo ucfirst( $status ); ?></strong></p>
 			<?php }
 			the_content();
 			get_template_part( 'template-parts/pag', 'post' );
