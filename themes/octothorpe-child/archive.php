31864071d40b5d0de08870582b7cdb4e65256d5e
Michael Beckwith <tw2113@gmail.com>
2019-03-30 23:39:13 -0500

    make individual posts more obvious and move time tag below the header

--- themes/octothorpe-child/archive.php
+++ themes/octothorpe-child/archive.php
@@ -1,31 +1,28 @@
 <?php
 /**
- * The home template
+ * The archive template
  *
  * @package Octothorpe
  * @subpackage Templates
  */
 
 get_header(); ?>
-<main>
-	<header>
-		<h1><?php echo esc_html( get_bloginfo( 'name' ) ); ?></h1>
-	</header>
-	<p><?php echo esc_html( get_bloginfo( 'description' ) ); ?></p>
-	<?php if ( have_posts() ) : ?>
+<main role="main">
+	<?php the_archive_title( '<header><h1>', '</h1></header>' );
+	the_archive_description();
+	if ( have_posts() ) : ?>
 		<ul class="post-list">
 			<?php while ( have_posts() ) : the_post(); ?>
 				<li <?php post_class(); ?>>
+
 					<?php the_title(
 						sprintf(
 							'<h3><a href="%1$s">',
 							esc_url( get_permalink() )
 						),
 						'</a></h3>'
-					);
-					echo do_shortcode( '[rt_reading_time label="Reading Time:" postfix="minutes" postfix_singular="minute"]' );
-					?>
-					<time datetime="<?php echo esc_attr( get_the_date( 'c' ) ); ?>"><?php echo esc_html( get_the_date() ); ?></time>
+					); ?>
+                    <time datetime="<?php echo esc_attr( get_the_date( 'c' ) ); ?>"><?php echo esc_html( get_the_date() ); ?></time>
 				</li>
 			<?php endwhile; ?>
 		</ul>
@@ -33,5 +30,5 @@ get_header(); ?>
 	else : ?>
 		<p><?php esc_html_e( 'No posts found.', 'octothorpe' ); ?></p>
 	<?php endif; ?>
-</main>
+</div>
 <?php get_footer();
