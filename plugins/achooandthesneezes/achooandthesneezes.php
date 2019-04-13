1aefa118c938c9f0e5d9dd99bd2e3a01710d9435
Michael Beckwith <tw2113@gmail.com>
2019-04-13 13:47:40 -0500

    trying out LET with js

--- plugins/achooandthesneezes/achooandthesneezes.php
+++ plugins/achooandthesneezes/achooandthesneezes.php
@@ -21,14 +21,14 @@ if ( is_user_logged_in() ) {
 ?>
 <!-- Matomo -->
 <script type="text/javascript">
-  var _paq = _paq || [];
+  let _paq = _paq || [];
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
-    var u="//trexthepirate.com/traffic/";
+    let u="//trexthepirate.com/traffic/";
     _paq.push(['setTrackerUrl', u+'piwik.php']);
     _paq.push(['setSiteId', 6]);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    let d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
   })();
 </script>
