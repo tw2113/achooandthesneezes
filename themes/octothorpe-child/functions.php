4eb06c26025c539c65e113e3860c5ef134fa85ea
Michael Beckwith <tw2113@gmail.com>
2019-05-04 21:42:31 -0500

    amend load order to put our compiled styles later

--- themes/octothorpe-child/functions.php
+++ themes/octothorpe-child/functions.php
@@ -8,7 +8,7 @@ function achoo_setup() {
 add_action( 'after_setup_theme', __NAMESPACE__ . '\achoo_setup' );
 
 function achoo_assets() {
-	wp_enqueue_style( 'achoo', get_stylesheet_directory_uri() . '/assets/css/sneeze.css' );
+	wp_enqueue_style( 'achoo', get_stylesheet_directory_uri() . '/assets/css/sneeze.css', [ 'octothorpe-style' ] );
 }
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ .'\achoo_assets' );
 
